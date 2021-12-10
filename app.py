96399625867a9d3b8ed02b02f77d35439c5f1221
Shay Poleg <shaypoleg@gmail.com>
2021-12-10 16:25:37 +0200

    Assignment 7 Submitted

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/home')
+def shalom_olam():  # put application's code here
+    return 'Shalom Olam!'
+
+
+@app.route('/homepage')
+def hello_home():
+    return redirect('home')
+
+
+@app.route('/articles')
+def hello_articles():
+    return 'Welcome to articles page'
+
+
+@app.route('/shaypo')
+def shaypo():
+    return redirect(url_for('hello_articles'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
