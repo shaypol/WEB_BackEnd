1c5b7e100a29920256d2795923dd57dc5def46c9
Shay Poleg <shaypoleg@gmail.com>
2021-12-21 19:10:31 +0200

    Assignment8 Submitted

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
 @app.route('/home')
 def shalom_olam():  # put application's code here
     return 'Shalom Olam!'
@@ -22,6 +21,23 @@ def hello_articles():
 @app.route('/shaypo')
 def shaypo():
     return redirect(url_for('hello_articles'))
+
+@app.route('/')
+def home():
+    return render_template('cv1.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('cv2.html')
+
+
+@app.route('/assignment8')
+def assignment8():
+    first_name = 'Shay'
+    last_name = 'Poleg'
+    return render_template('assignment8.html', first_name=first_name, last_name=last_name,
+                           hobbies=('Basketball', 'Pilates`', 'Napping'))
 
 
 if __name__ == '__main__':
